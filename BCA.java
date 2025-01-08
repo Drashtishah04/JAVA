4f85857db0115091c2a8aadc4c8017f6a8d8d8c5
Drashtishah04 <141635058+Drashtishah04@users.noreply.github.com>
2025-01-08 16:54:35 +0530

    Add files via upload

---
+++ BCA.java
@@ -0,0 +1,7 @@
+class BCA
+{
+    public static void main(String s[])
+    {
+        System.out.println("My name is :"+s[0]);
+    }
+}
---
+++ doubledatatype.java
@@ -0,0 +1,10 @@
+class double1
+{
+    public static void main(String p[])
+    {
+        double a,b;
+        a=23.2;
+        b=3.5;
+        System.out.println("sum= "+(a+b));
+    }
+}
